c73fb6b7109471667c08f7f724a8af973bc080e0
Mr-Aaryan <kc.aryan3536@gmail.com>
2024-04-09 13:34:24 +0545

    nav & footer updated

--- src/lib/components/Recents.svelte
+++ src/lib/components/Recents.svelte
@@ -2,7 +2,7 @@
     import RecentCard from "./cards/RecentCard.svelte";
 </script>
 
-<section class="py-4 pb-8 border-b border-gray-400">
+<section class="py-4">
     <h2 class="text-3xl pb-1 my-4 border-b border-black w-fit">Recent Recipes</h2>
     <div>
         <RecentCard />
