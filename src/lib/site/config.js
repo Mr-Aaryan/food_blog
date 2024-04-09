c8d501186d047357f4d43ccd36d5307ddd1fc71a
readcache0 <readcache0@gmail.com>
2024-04-09 11:19:55 +0545

    added essential files

---
+++ src/lib/site/config.js
@@ -0,0 +1,7 @@
+import { dev } from '$app/environment'
+
+export const siteName = 'foodblog'
+export const siteUrl = 'https://foodblog.vercel.app/'
+export const siteTitle = 'Food Blog'
+export const contactMail = 'foodblog@gmail.com'
+export const url = dev ? 'http://localhost:5173/' : 'https://foodblog.vercel.app/'
