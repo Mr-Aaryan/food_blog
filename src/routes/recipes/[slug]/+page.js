75ca48ccb1a371a9f835fe5ba575733a80ff1d78
Mr-Aaryan <kc.aryan3536@gmail.com>
2024-04-12 12:48:09 +0545

    category

--- src/routes/recipes/[slug]/+page.js
+++ src/routes/recipes/[slug]/+page.js
@@ -1,14 +1,16 @@
 import { pb } from '$lib/pocketbase';
 
-export async function load(){
-
+export async function load({ params }){
+    
+    const categoryId = params.slug;
     const posts = await pb.collection('posts').getList(1, 50, {
-        filter: 'Published = True',
+        filter: `Published = True && category = '${categoryId}'`,
         expand: "category"
     });
-
+    
     console.log(posts);
-
+    console.log(categoryId);
+    
     return {
         posts : posts.items
     }
