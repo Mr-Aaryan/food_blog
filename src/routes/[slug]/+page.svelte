19d3a2edf1adc86753d99ecff6623fa0579b493d
Mr-Aaryan <kc.aryan3536@gmail.com>
2024-04-14 23:40:59 +0545

    bug fixed

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -83,7 +83,7 @@
         <div class="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2 xl:grid-cols-3">
             {#each data.similar_posts as post}
             <a href="/{post.title.toLowerCase().replace(/\s+/g, "-")}-{post.id}" class="">
-                <img class="object-cover object-center w-full h-64 rounded-lg lg:h-80 ease-in duration-100 hover:scale-105" src="http://127.0.0.1:8090/api/files/{post.collectionId}/{post.id}/{post.featured_image}" alt="{post.title}">
+                <img class="object-cover object-center w-full h-64 rounded-lg lg:h-80 ease-in duration-100 hover:scale-105" src="https://pramilasrecipes.pockethost.io/api/files/{post.collectionId}/{post.id}/{post.featured_image}" alt="{post.title}">
                 <div class="py-2 hover:none">
                     <span class="uppercase text-sm font-semibold text-gray-700">{post.expand.category.category}</span>
                     <h2 class="text-xl text-yellow-500 hover:underline">{post.title}</h2>
