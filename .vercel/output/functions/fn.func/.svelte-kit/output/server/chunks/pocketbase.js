70de0c07f4e4fdaab581486b189b0875789aac88
readcache0 <readcache0@gmail.com>
2024-04-15 12:10:49 +0545

    rm vercel

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/pocketbase.js
+++
@@ -1,6 +0,0 @@
-import { P as PUBLIC_PB_URL } from "./public.js";
-import PocketBase from "pocketbase";
-const pb = new PocketBase(PUBLIC_PB_URL);
-export {
-  pb as p
-};
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/public.js
+++
@@ -1,4 +0,0 @@
-const PUBLIC_PB_URL = "https://pramilasrecipes.pockethost.io";
-export {
-  PUBLIC_PB_URL as P
-};
