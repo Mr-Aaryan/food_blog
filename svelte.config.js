6f40347a7a9c3766525b94aa2bfd14a2904b7558
readcache0 <readcache0@gmail.com>
2024-04-15 12:04:01 +0545

    version

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   kit: {
     adapter: adapter({
-      runtime: 'nodejs18.x',
+      runtime: 'nodejs18.2',
     }),
   },
   preprocess: vitePreprocess()
